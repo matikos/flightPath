b575631c5e4070f71fd0d95f7dfe90114ab2d29b
matikos <mati.kos@me>
2019-05-14 19:46:24 +0100

    first commit

---
+++ flightPath.js
@@ -0,0 +1,92 @@
+let flightPath = "L---+++- L-++---++ L+ R--++------ L- L- L--- L- R+ R-+ L+ R++- L- R-+ R- L+ L- L- L-- L++ L---++---+ L--++ L- L- R- R+ L-++-- R-+-- L+ L+++--+- R+ L+ R+ L- R+- R- L- R-- L- L+++--+++ R+ R-++- R-- L-++ L- R--+ R+---+ R+ R- L+ R- R--- R-- R+-+--+-++ L- R--++ R- R++---- L+ R- L- L+--+-+- L--+- L- L+ L- R+ L- R- L-+ L- L- L- R- R- R+- R--+----+-+ L+++-+-+--+ L-+-+ R+ L+--- L++- L-----+++++ L- L+ L+ L+ L- L- R- R- L----+--- R-+ L- L++- L+-+--++ R+ L- R+ R+--+- R+-+++ R++-- L- R+ R- R--+---- R+ R- L+ R++------- R-+ L+ L+ L-++ R--+ L+ L- L- L+ R+ R+++- R- L- R- R- R- R- R+- R- L++ R- R+ L- R-- R- L+ R- L+ R- R- L- L-+- L- R- R+ R- R+ L- R-- L--++ L- R+- R- R- R----++ R- L- R++++ L+- R+ R+- L- R-++ L- R- R- L-++- R- R+ R+-- L+++++ R- R- R+ L+-+-- R+ L+-+--+- R+ R+- L+++--- R+ R- L- R+ L+-++-- L--++-+-- R- L- R+ R-+ L+-- L+ R--+ R- R- L- R++- R+ L- L- R+ R+ L+-+ R+ L+- L- R--+-++ L+ R+ L+ R--++-+-- R-++-+ R+- L-+ R- L+++- L+ R--++--- R+- L- L- R+----+-+ L- R- L+ L- L- R- L+ R- L++ L+ L+-+++ L+ R+ L- R-++ L+ L-+- R++++ R+ L+ R+ L- L+ R+--+-+ R-- R- L- R- R- L- R+ L+- L+++ R+-+ R- L-- L- R+ R-++--+ L-- R-+ R++-- R+ R-+ R-++ L- L+-+ L+ L+ R+++-- L+ R---++-+ L++- L++-+-- R+- R--+ L++ R- R+ R+-+ L+ L- R-- R+ L+- L- L- L+ L++-+ L-++-+ R+- R-++ L-++- R- R--- L+ L+- R++-- R+ R+ L+++- R- L- L-+++--- R- R- L- R+-- R- R+--++ L--+ L+ R- R- R- L+- R+ L---++-- L+ R-+---+--- R++ R+ L+ R+ L++++ R-- R+ R+---- R- L+-+-+- L-- R-+- R- R- R-- L-- L+- R+ R- R+ L+ L-- R- L+-- L++++++ R- R+ R+ L- R+ R+ R+---+ R+ L- L+ R- R+- R++++ L+--+ R-+-- L+ R- R- L+ R-+-+ R+++ L+ R-+ R-+-+-- L- L++++ L+ L+ R+ R--+-+ L- R+ R- L+ L-++++ L+ R- L+ R+++ R++ R++ L+- R+ R- R+--+-- L-+ L-+- R- R-+ L- R+--++++ L+ R+- R+ L- R++ L- R+ L++--+- L--++-- L+- L-+- R+ R++- L+ L---- L+ R++- L-- L-+-++ R- L++++ L-+ L- L-+ R+ L++-+-+-+-+- L- L+ L+ R- R+++- R-- R- L+ R-+++++-- R+ L-- L+ R-+-+ L-+ R--- L+ L-++-+ R+ L- R- R- R- R+ R+ R+ R-- R-+--+ L- L+-+ L-+ L+++++ R+--++- R-++ L-++- R-+ R+ L- L-+- R- L-+++ R- R-++- L+-+ R- L+ L--++- L- R-+++--- L--- L++ R++--+--+ R+-- L- L- R+ R+ L+ R--+ L- R+-+ L+ R++--+-- L+ R-++ R- L+ R-+- R- R+- L+ R+ L+- L+ L+ L---+ L- L+ L+ L+ L- L+ L- R+ L+ L- R- L-- L-+ R+++++ R+ L- R- R-- L- L- L+-++ R-- L- L+ R++- L+- R- R- L+ R+ L++ R- R+ L+ L- R-- R+ R- R- R+--++-++ L- R-- L+ L+ R+ L+ R++ L- R-+ L+ L- L+-++-- L+ L-+ R+ R++- L-- R++ R- L+ L+- L- R+ R- R-+ L+ L+ R++ R- R- R-++- L++--++++++ L- R+ R- L+ L+ L+ R- L--+++ R+ R-+----++ L--- L-+ R+-+ R+ L- L-++ L+ R+ L-- R+ L+++--+-+- R++--+- R- L++ L- R+ L+-- R+ R++++ R+ L+ L++ R- L+--++-++ L+ L+--+++- L-- L-+- R++- L- L+ L+ R- R++-+- L+ L--- R- L- L+ R---- R+ L-- R- R- R+ L+ L-- R+ R+ R+ L- R+ L- R- L-+ L+ R-- R+ R--++ L- L--++-- L+--- R+ L+ R-+ R++-- R+ L- R- R- L+ L--++- L- R+- R-+- L+ R+-+-+++ L+ L- L+ R+++ R- R++ R-+++ R+++-+ L- R+ L+-+ R+ L+ L+-+ R+ L+ L- L- L+++---- L+ L+++ R+--+--+++---- R- L- R- L-+ R+ R+ R+-- R- L+---+- R-+-+- L- L-+- R- L+- L--+- R- L- L+ L+ R+ L--++- R- R- L+++++ R- L++ L+ R- R-+ R+ L+ R++ L+- L- R++----+-++--+- R+-- R---+ R+- L+ L++- R- R- L+ R+ R- L-+ L-+- L+- L-++ L- R-++ R-+++++- L----- L+ R- R-++ R-+-- L---+ L+ R++- R+ R+- R- R++- L+ L- L++- L- R+ R++--+----+-- L--++ L+ R-- L- R++ R-- R-++ R++- R+ R++ R+++++--- R+ L-- L-+++ R+ L+++- R- L++ L+ L+ L-+ R--++- L+ L- L+- L--+++++ L-++ R+ L--+ R- R- R--+ R-+++ L- R- R+ L-+ L+-----+-+--- L++ L+- L+ L+ L-+ L- R+ L-++ L+-++++ R+ L- L++-- L- L- R-+ R+ L- R- L---- R-++-+ L- R----++---+- L- L++ L- L- L+- L+ R--++-- R-+-- L-+-++ R-- L- L+++-+++--+- R+- L-----+----+- L+- R-+- R+ R+++- L-++-- R+- R+ L-- R- L--+++-+ R+ L+ L- L-+-+-+- L-+++-++ L--+-- R- R--+ R--+ R--+ R+ L- R++-++++++-+- L+++--- L---+ L- L-- L+ R- R+ L- R+++-+-++--+- R- R+ L+ R+ R+ R+ L+ L-+ R-+ R--- R+-- L+ L- L- R- R+ R+ R++ R+ R- L+ L- R++ R+ L+ L++ R+++- L--- L+ L+- L-- R+ L- L--+ L-+-+ R-+--+-+ L++--+ R++++ R+++-- R- R- R- L+ L- R+ R- L+ R- L- R+ R++ R++-+ R- R- L-- L- R++ L- L+++-+- L-+ L--+ R+ L+ L- R+ R+-- R-+-+--+--+ L+ L+ R- R+ R----+ R-+ L+ L- R-++++ R---+-+ L-++-+ L+ R+ L- L---+ R--+ L+ R- R+ R++ R+--++-++ L+ L+ R+ R- R- R-++++ L- L+-+---+---- R+ R- L+-+ R+ R+ L- R-- R+-+---+- R+ R+ R- R---+-++++-- R++ L-+--++ L- L- L- R+ L+ L+-+ R- L++--+++-+ L+ R-+ R+ R- R++ R- L---- R+ L+ L- L- R+-+ R---++-+-+ L-+- R+---+-+ L-+- R--+- L+- R++++- L- R++ L+-+ L+-- L- R-+--+ L- L-+ R+ R+ L-++ L- L+ L- R-+-- L-+ R- R+ L- L--+ R- L-- R-++- R-++ L++ R++--- R- L- R--- R+- R-";
+
+//Breaking down flight path into sequence of moves
+let moves = flightPath.split(" ");
+
+
+//placing drone on the map
+let drone = {
+  x: 0,
+  y: 0,
+  compass: 'N',
+  thrust: 1
+}
+console.log(`Take off! Drone begins the flight from position ${drone.x},${drone.y}`);
+
+const leftTurn = () => {
+  switch (drone.compass) {
+    case 'W':
+      drone.compass = 'S';
+      drone.thrust = -1;
+      break;
+    case 'S':
+      drone.compass = 'E';
+      drone.thrust = 1;
+      break;
+    case 'E':
+      drone.compass = 'N';
+      drone.thrust = 1;
+      break;
+    default:
+      drone.compass = 'W';
+      drone.thrust = -1;
+      break;
+  }
+  return drone.thrust
+};
+
+const rightTurn = () => {
+  switch (drone.compass) {
+    case 'W':
+      drone.compass = 'N';
+      drone.thrust = 1;
+      break;
+    case 'S':
+      drone.compass = 'W';
+      drone.thrust = -1;
+      break;
+    case 'E':
+      drone.compass = 'S';
+      drone.thrust = -1;
+      break;
+    default:
+      drone.compass = 'E';
+      drone.thrust = 1;
+      break;
+  }
+  return drone.thrust
+
+}
+
+const stepsValue = (m) => {
+  // number of steps in a move
+  let v = 0
+  for (let i = 1; i < moves[m].length; i++) {
+    if (moves[m].charAt(i) == '+') {
+      v += 1;
+    } else if (moves[m].charAt(i) == '-') {
+      v -= 1;
+    }
+  };
+  //move direction
+  if (moves[m].charAt(0) == 'L') {
+    leftTurn();
+  } else if (moves[m].charAt(0) == 'R') {
+    rightTurn();
+  };
+  return v * drone.thrust
+};
+
+const moveDrone = () => {
+  for (let i = 0; i < moves.length; i++) {
+    let step = stepsValue(i);
+    if (drone.compass == 'W' || drone.compass == 'E') {
+      drone.x += step
+    } else {
+      drone.y += step;
+    }
+  }
+  console.log(`Game Over. You safely arrived at your final destination : ${drone.x},${drone.y}`);
+}
+
+moveDrone();
