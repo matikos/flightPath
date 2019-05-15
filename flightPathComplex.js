312d5a2132aa0859f66a4e4ff6b4bf7ff69facac
matikos <mati.kos@me>
2019-05-15 16:00:21 +0100

    added the task 2

---
+++ flightPathComplex.js
@@ -0,0 +1,110 @@
+let flightPath = "L+ L- R-+- R-+-+ L- E++ R-+ L- L+ L+ R+- R+ L+ R---+- E- N+++ R+- L- E- R-+-- N- R+ R- L- R+- L+ L+ L+ L-- S+ L+-+- R----++++ L+ L-+ R++ R-+++-+- R- R----- L-++ W- W- L+++- R+- L+ W- R- L-+ R+-+ L+++- L- W- R+ R- E+---+ W++- R+ N+- L-+-- R+ L++- R+ W-- L+ R-- S+- R-+ R--- L-+-++- L- L+ R+- S- R+ L+ R-+----++ R- S-+++ R+ L+-- L+ R+-+-+ R- N- E+ L-+--+ R+ R++ L+-+ S+ R+--- E+- N-- N- E+ R- L-- R+ N- L+ L- L----+ W+--- R- L- N+ L---+++ E+- R--+++-++-+-+ L-+--+ L-++++++-- L-+ L+--- S- L- R-- L+ R++-+++ R+-- R++- R++ E- L++-+-- R- L++++ R+ R+++- L- R+ R+ R+ S- L+ R++++- L---+ E- R+ R-++- N-+- E++ R-+ W--- R- L+ L+ L++++-+ L+-+--+ R+ L+--+ L-- N- L+ N--+-+--+-+- N-- L- R+--+ R+ R+ R+++- L+ L- L-+-+- W--++- E- W- N+- L+++++ N--- R+ L+ S+-+-- R- W+ R+---+-- S- R-+++ L- S-+ R- L+ L- L+ L+--+ L- N--++ N++-+++-- R-+++- R+ W+ E+ L+ L- S- W- R+ S+-- L+ R+ R- L- R- R- R++ S--- S+ S--+ L- R++- R++- R+ L- E- R-- R- L+++ S++-+-++--+ W- W+ R+ R++ L+ L-++-+ W+ R--+-+++ R-++++- L+ L- R+++-- S+ R+ L- W- R- L- R-+-+----+--- L- E- L+-++-+-++ L-++-+ R- L-+-+++- R+ R+ N--+ S- E- L+--+--+-+ W--++ R+ L- W+ R-- S-+- R++--+ L- L+ E+ L- R-+- R---- R+ R- W+ L+-+- S- R+ L+++- L+++ R+ R- N- L+ R+--+ L+ R+ L- L- L+--+-+ R- L- R+ L-+ L+ E++ R- L- R+++ E+ R--++----- L-- N- R++-++ L+-+- S- L- L-+ R-- L- R- L- N+ L+ R--++++++ R++-+- L+ R- R+ E---+-+ R+ S++-+++- S---+-- L+ L+ R+-+ N- L+- L--+ R+- E--+++ R+ L+-- W- S- L+ R+ R- L-+- W-++ N- R- R-- L- L- E- R--- L+ S-- R- R-- R+++--++---+ L-------+ E+ N++ W-+++ N-++ L+ N-- L-+---- R+ R+-- L- L+++ L+ R- L++++++- L- L+----- R- R+ S+ S- L-+ L- R+ L+ R- W- L-+ R-+- E+ R++ L+ E--++ R- L+ L+ L+ W- R- R--+- W- R- R- R-+ N- S+-+ L+ R-+---++- E++ L+ L+++++ R+ R+-++-++------+ R+ W+ W---++ R-++ S+ R- L+ L-- L- L++-+-++++ L- R-+++- R+ R-- S-- R+ R+ L+-++ S- R+ L- R+ L-+-- R+ R+ R+--+ L+ R---+++ R----+- L-- R- N- S+ L+ R++---++--+- S- R- L+-+++ R- R+++- L+ L+ R--+ S++ R- L+ L-+-+- R--- L++ W+- R+++- S- E++ L-----++--- L+ L- L+-+ L- L++-- R- W+- L- R-+-- E+ L- R- R+ L-- R+ N+ R+--- L+- R- R++--- L+ R---+ R+- R+ R- R++-+-+ S-+-- R- L+++-++-+ L- E+ L- R+-- L+ R+++ W-+ L-+ R-+ R-++-+++ L- L- R++ E- N++ E++ R--++ R- R+ R- L- W+ R+ R++-- N- L+-++- R-+---+- N- L++ R-+---+ R- R+- L+ L++++- R-+- W--- S+ L++- S- S++ E--- R+ S++-- W-++++ R+ L++++ L- W+ S+ W- R+-- E- L+-++----+++ S+- R- R-- L+-+ L-- W-- L- L- L- W-+ R+ S+ L- R+-++- N+ R++-+-+--++- N- S+ N- L- L++- R+-- L- R- R- L++- R++++ R-+ R+-+++- L++--+++-+ N+ S--+-+ L- L+ L+ L+ N-+ R- L----+ L- L+ N+ R- L+- R+ L- R++- W- L- E-+--+ R+ R-+ W+++ R+ R+-- W- L-+ S- R- W- L+-+ E+ R- E- S+ N+ N-- L-++- R+ R+ E- R+ L+ R+- R- R- N- L- W- R+ E- L- N+ W+--+- L++++-++ R- N+ L- E+- S- R+ N+ N+ L+ L+ R+ R- N+ L- R-------+--++--- R- L- R- S-+-+--- R- L-+ R+ N-- L+ R+- R- N+ W++ S++++++-++ E+- L++- R++++--+-++-+ R-+ R- R+ R- R- R+ R- R+ L+ N+++ R-+- R---- R+--- R-+--++ L-- L-+ W-+ L++ L- E----+- R--- E+ R+ L--- L+++ E- W- R-+ L---- R-+-+ W+ L+- R+ L-+- L++-+--+ L- L+ W+ L+ E-+- L--+-++--- R+ N++-+ R+-+-+-- L++++ R--+-+ L+ L- W++ L-+ L+ L- R-- L+ E- L+ E+ L---+ R+ E++++ R+ E+ N+ R+-+---+ N+---+- R--+- R+ L+++ L- L+ R+ R- S- R- L++- L+- L---- S+ R+- R- R- R- R+ L-+- L--+ E++- L- L- R- R+ L-+++ E--+-+ R--- L+ S+ N-- L- L+++ R-+ L+--+ R- E- L- R- W- N++- L- R++ R+++ R++ N+ L+++--+ W+-+ R+ L- L- R++++ L+ L-+ R- L-- R++++- E- L-+-+++--+++- L+-+- R+ L+-++ R- W- R+ L+ L+ L+ L+- R- R+-++-- R----+-- R+ R-- W+ S- E+ R-+ L+ L- R+ R+ L- W- S+-++-+ L- L- S+-+ E++ W++- R+ R- R- L-- R++- R-- R- L- R-++ W+ L- L---+ L- R+ R+ R++ S-+- R++-+--++ L-+++-- W- S- R-++ L+-- R---++-+++ S+ L+ E++ N- S+- L+ L++ R- E+ R+ L+-- N+-+- L+ R+ W++- L- L+++ E- L+ R- L-+-- R- R---- L--+ L--- R-+ E++-+--- L+ L- L++-++ W- R- L---- L- R- L+ E- S- R- R-+++ R--+ N-+--- E+-----+ L+ N++++-- L-+-+ R+ R+- R-+ L- R- S+ L+ L++-+ R+- R+-+ W++++++- R- E+ R++ E---+-+ R- R- R+ R+ R++--+ R+ R--+ W-++-++++- L- L- S-+- R--+ L+ N- R+ E++ W-+- N+ L- S++ L+--+++ R+- L- R- W+ W+ S++ R++-++++++-++ S--+ L+ S+ E+ R-+-+- L----+-- S++ W+++-- L- R- R-++ R+-+ R- E+ L+ R- W- E++ N-+ L- R+ R+ W-+--+- E- R-+ S+ R+ R+ R-+--+--+ L+ R-- R-+++-++++++-- L+ W-+ L-+-+-++ L-++-- R-- E---+ L-- L-+ W- R-+-+-+ R+ W- L+ R+ L- L- L-+-++ L- W+ N+- L+--+--+ S-+ W++ L+++- R- W++++ E++--+ R-++-++ L- R- R+++-+-+-+ R- S- L- N- L+++++- R- R- L- R+ N+ R+ R+ N- S--+- N+ R+ L+-++ R-+ L+ E-+-+--+ L+ E- R+ L- N+--+-+ R+ L+ R+--++-- L+ N- L+-+-+++ E+ R+ N++ R-+- N- W+-+ L- R+ R+ E++ L-- R--++- N- R+ R- R- S+";
+
+//Breaking down flight path into sequence of instructions
+let instructions = flightPath.split(" ");
+
+
+//placing drone on the map
+let drone = {
+  x: 0,
+  y: 0,
+  facingDirection: 'N',
+  movementDirection: 'N',
+}
+console.log(`Take off! Drone begins the flight from position ${drone.x},${drone.y}`);
+
+const leftTurn = () => {
+  switch (drone.facingDirection) {
+    case 'W':
+      drone.facingDirection = 'S';
+      break;
+    case 'S':
+      drone.facingDirection = 'E';
+      break;
+    case 'E':
+      drone.facingDirection = 'N';
+      break;
+    default:
+      drone.facingDirection = 'W';
+      break;
+  }
+
+  return drone.facingDirection
+};
+
+const rightTurn = () => {
+  switch (drone.facingDirection) {
+    case 'W':
+      drone.facingDirection = 'N';
+      break;
+    case 'S':
+      drone.facingDirection = 'W';
+      break;
+    case 'E':
+      drone.facingDirection = 'S';
+      break;
+    default:
+      drone.facingDirection = 'E';
+      break;
+  }
+  return drone.facingDirection
+
+};
+
+const orientationFinder = (m) => {
+  if (instructions[m].charAt(0) == 'L') {
+    leftTurn();
+  } else if (instructions[m].charAt(0) == 'R') {
+    rightTurn();
+  };
+  return drone.facingDirection;
+};
+
+//establishing move direction
+const moveDirection = (m) => {
+  orientationFinder(m);
+
+  if (instructions[m].charAt(0) == 'L' || instructions[m].charAt(0) == 'R') {
+    drone.movementDirection = drone.facingDirection;
+  } else {
+    drone.movementDirection = instructions[m].charAt(0);
+  };
+  
+  // movement intended along an axis: 1 increasing position's value, -1 decreasing position's value
+  if (drone.movementDirection == 'N' || drone.movementDirection == 'E') {
+    return 1
+  } else {
+    return -1
+  }
+  
+};
+
+const stepValue = (m) => {
+  // number of steps in a move
+  let step = 0
+  for (let i = 1; i < instructions[m].length; i++) {
+    if (instructions[m].charAt(i) == '+') {
+      step += 1;
+    } else if (instructions[m].charAt(i) == '-') {
+      step -= 1;
+    }
+  };
+  //final move calculation
+  return step * moveDirection(m)
+};
+
+const moveDrone = () => {
+  for (let i = 0; i < instructions.length; i++) {
+    let step = stepValue(i);
+    //applying step to proper axis
+    if (drone.movementDirection == 'W' || drone.movementDirection == 'E') {
+      drone.x += step
+    } else {
+      drone.y += step
+    }
+    
+  }
+  console.log(`Game Over. You safely arrived at your final destination : ${drone.x},${drone.y}`);
+}
+
+moveDrone();
